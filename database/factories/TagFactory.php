d011c5e517f53051d1ae54b7866f4e458ab165c5
serhiy <sergey@uroboros.Dlink>
2022-05-05 16:34:22 +0300

    [jb-2] seeders realized

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = ucwords($this->faker->word);
+
+        return [
+            'name' => $name,
+            'slug' => \Str::slug($name),
+        ];
+    }
+}
