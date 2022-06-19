0220a0d552857fe05746f799587e4906b4942f6e
zeniemand <serhiy_niemand@protonmail.com>
2022-06-19 17:41:51 +0300

    [jb-4] listing search front page

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+mix.webpackConfig({
+    stats: {
+        children: true,
+    },
+}).js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
