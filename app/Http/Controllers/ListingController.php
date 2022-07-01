d72ee9758d5c351854bc123493690fd14cec6f0c
zeniemand <serhiy_niemand@protonmail.com>
2022-07-01 10:15:37 +0300

    [jb-6] Listing logic and View for purchase and display

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class ListingController extends Controller
@@ -63,4 +66,125 @@ class ListingController extends Controller
             ]);
         return redirect()->to($listing->apply_link);
     }
+
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate($this->getListingValidationArray());
+        $user = $this->getAuthStripeUser($request);
+        return $this->processPayment($request, $user);
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getListingValidationArray(): array
+    {
+        $validationArray = [
+            'title' => 'required',
+            'company' => 'required',
+            'logo' => 'file|max:2048',
+            'location' => 'required',
+            'apply_link' => 'required|url',
+            'content' => 'required',
+            'payment_method_id' => 'required',
+        ];
+
+        if(!Auth::check()){
+            $validationArray = array_merge($validationArray, [
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed|min:5',
+                'name' => 'required',
+            ]);
+        }
+
+        return $validationArray;
+    }
+
+    /**
+     * @param Request $request
+     * @return object
+     */
+    private function getAuthStripeUser(Request $request): object
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+
+            ]);
+
+            $user->createAsStripeCustomer();
+
+            Auth::login($user);
+        }
+
+        return $user;
+    }
+
+    /**
+     * Process payment and create new Listing
+     * @param Request $request
+     * @param  $user
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    private function processPayment(Request $request, $user)
+    {
+        try{
+            $amount = 4900;
+            if ($request->filled('is_highlighted')) {
+                $amount+= 900;
+            }
+
+            $user->charge($amount, $request->payment_method_id);
+            $listing = $this->createPayedListing($request, $user);
+            $this->attachTagToPayedListing($request, $listing->id);
+
+            return redirect()->route('dashboard');
+
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
+    }
+
+    private function createPayedListing(Request $request,User $user)
+    {
+        $md = new \ParsedownExtra();
+        return $user->listings()
+            ->create([
+                'title' => $request->title,
+                'slug' => Str::slug($request->title) . '-' . rand(1111, 9999),
+                'company' => $request->company,
+                'logo' => basename($request->file('logo')->store('public')),
+                'location' => $request->location,
+                'apply_link' => $request->apply_link,
+                'content' => $md->text($request->input('content')),
+                'is_highlighted' => $request->filled('is_highlighted'),
+                'is_active' => true
+            ]);
+    }
+
+    private function attachTagToPayedListing(Request $request, $listingId)
+    {
+        foreach (explode(',', $request->tags) as $requestTag) {
+            $tag = Tag::firstOrCreate(
+                [
+                    'slug' => Str::slug(trim($requestTag))
+                ],
+                [
+                    'name' => ucwords(trim($requestTag))
+                ]
+            );
+
+            $tag->listings()->attach($listingId);
+        }
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ListingController::class, 'index'])->name('listings.index');
 
+Route::get('/new', [ListingController::class, 'create'])->name('listings.create');
+
+Route::post('/new', [ListingController::class, 'store'])->name('listings.store');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
